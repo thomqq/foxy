4ce255656950548d9a5b8c9e07da1ab2fdc39700
Arkadiusz Biczewski <arkadiusz.biczewski@itsoft.pl>
2015-12-17 10:22:43 +0100

    Ładne URLe dla przeglądarek HTML5

--- ASList/assets/js/app.js
+++ ASList/assets/js/app.js
@@ -2,15 +2,17 @@
 
 var aslistApp = angular.module('ASListApp', ['ngRoute', 'ui.bootstrap']);
 
-aslistApp.config(['$routeProvider',
-    function ($routeProvider) {
+aslistApp.config(['$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider) {
         $routeProvider.when('/', {
             templateUrl: '/templates/login.html',
             controller: 'LoginController'
         }).otherwise({
             redirectTo: '/',
             caseInsensitiveMatch: true
-        })
+        });
+        
+        $locationProvider.html5Mode(true);
     }]);
 
 aslistApp.controller('LoginController', ['$scope', 'AuthenticationService', function ($scope, AuthenticationService) {
