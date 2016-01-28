8527a7847a401578078811427e633db494df1a21
Arkadiusz Biczewski <arkadiusz.biczewski@itsoft.pl>
2016-01-28 11:28:54 +0100

    Use strict - zabezpieczenie przed undefined

--- ASList/assets/js/service/AuthenticationService.js
+++ ASList/assets/js/service/AuthenticationService.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function () {
     
     var aslistApp = angular.module('ASListApp');
--- ASList/test/unit/policies/Roles.test.js
+++ ASList/test/unit/policies/Roles.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var request = require('supertest');
 
 describe('Policies', function () {
